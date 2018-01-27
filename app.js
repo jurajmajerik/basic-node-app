f73be0a45fa7a055cb19e0be61040320fbe1c842
jurajmajerik <juro.majerik@gmail.com>
2018-01-27 18:31:59 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+app.get('/contact', function(req, res) {
+  console.log(req.query);
+  res.render('contact', {qs: req.query});
+});
+
+app.get('/profile/:name', function(req, res) {
+  var data = {name: "Juraj", age: 26, job: 'developer', hobbies: ["cycling", "running"]};
+  res.render('profile', {person: req.params.name, data: data});
+});
+
+app.listen(3000);
